1783a26f082e1700ddb67423c0d9f431d0c6fcf3
nrlsolomon <nrlsolom5@gmail.com>
2018-06-21 21:37:59 +0000

    automatically backed up by learn

---
+++ rivers.rb
@@ -0,0 +1,16 @@
+class Rivers 
+  attr_accessor :theNile, :theMississippi, :theAmazon, :theSeine, :theYangtze, :theEuphrates
+  
+  def initialize(theNile, theMississippi, theAmazon, theSeine, theYangtze, theEuphrates)
+    @theNile
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
